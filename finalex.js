bf9ed1e39c9eab6de847d126d76cb0fbd658ec80
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-02-15 05:45:06 +0300

    Add files via upload

---
+++ finalex.js
@@ -0,0 +1,25 @@
+const a = true;
+if(!a) {
+    console.log("Green");
+} else {
+    console.log("Blue");
+}
+var x = 2;
+x += 5;
+console.log(x);
+var result = 0;
+
+var i = 0;
+var limit = 3;
+while(i < limit) {
+    result += 2;
+    i++;
+}
+
+//console.log(result);
+
+var cat = {}
+cat.sound = "meow"
+var catSound = "purr"
+console.log(cat.sound)
+
